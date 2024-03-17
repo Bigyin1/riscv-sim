7f4f77449b8e7b7e5c63bf7364cd13da70f7457b
Bigyin1 <sergei.aparin1955@yandex.ru>
2024-03-17 17:43:23 +0300

    added googletest

--- tests/cpu_test.cpp
+++ tests/cpu_test.cpp
@@ -1,91 +1,89 @@
 #include "cpu/cpu.hpp"
 
+#include <gtest/gtest.h>
+
 #include <iostream>
 
 #include "ram/ram.hpp"
 
-static const riscvModel::Instruction* nextInstr(riscvModel::CPU& cpu, riscvModel::RAM& ram)
+class TestArithm : public testing::Test
 {
-    static size_t loadAddr = 0;
-
-    auto ri    = ram.ReadWordFrom(loadAddr);
-    auto instr = cpu.Decode(ri);
-    if (instr == nullptr)
+protected:
+    const riscvModel::Instruction* getNextInstr()
     {
-        std::cout << "failed to decode" << std::endl;
-        return nullptr;
+
+        auto ri    = ram.ReadWordFrom(loadAddr);
+        auto instr = cpu->Decode(ri);
+        if (instr == nullptr)
+        {
+            return nullptr;
+        }
+
+        loadAddr += sizeof(riscvModel::RawInstr);
+        return instr;
     }
 
-    loadAddr += sizeof(riscvModel::RawInstr);
+    virtual void SetUp() override
+    {
+        std::ifstream bin("arithm.bin");
+        ram.LoadCode(bin, loadAddr);
 
-    return instr;
-}
+        aspace = new riscvModel::AddrSpace(&ram);
 
-int main()
+        this->cpu = new riscvModel::CPU(regs, *aspace);
+    }
+
+    virtual void TearDown() override { delete cpu; }
+
+    riscvModel::RAM       ram  = {0};
+    riscvModel::Registers regs = {0};
+
+    riscvModel::AddrSpace* aspace = nullptr;
+    riscvModel::CPU*       cpu    = nullptr;
+
+    uint64_t loadAddr = 0;
+};
+
+TEST_F(TestArithm, test1)
 {
-
-    uint64_t              loadAddr = 0;
-    riscvModel::RAM       ram(loadAddr);
-    riscvModel::Registers regs(loadAddr);
-
-    std::ifstream bin("arithm.bin");
-    ram.LoadCode(bin, loadAddr);
-
-    riscvModel::AddrSpace aspace(&ram);
-
-    riscvModel::CPU cpu(regs, aspace);
 
     std::cout << "testing: "
               << "addi  x10, x0, 1" << std::endl;
-    auto instr = nextInstr(cpu, ram);
-    if (instr == nullptr)
-        return 1;
+    auto instr = this->getNextInstr();
+    ASSERT_NE(instr, nullptr);
 
-    cpu.Execute(instr);
-
-    if (regs.ReadRegVal(10) != 1)
-    {
-        std::cout << "got: " << regs.ReadRegVal(10) << "wanted: 1" << std::endl;
-    }
+    cpu->Execute(instr);
+    ASSERT_EQ(regs.ReadRegVal(10), 1);
 
     std::cout << "testing: "
               << "addi  x10, x10, -2" << std::endl;
-    instr = nextInstr(cpu, ram);
-    if (instr == nullptr)
-        return 1;
+    instr = this->getNextInstr();
+    ASSERT_NE(instr, nullptr);
 
-    cpu.Execute(instr);
-
-    if (regs.ReadRegVal(10) != (uint32_t)-1)
-    {
-        std::cout << "got: " << (int32_t)regs.ReadRegVal(10) << "wanted: -1" << std::endl;
-    }
+    cpu->Execute(instr);
+    ASSERT_EQ(regs.ReadRegVal(10), (uint32_t)-1);
 
     std::cout << "testing: "
               << "add x12, x0, x10" << std::endl;
-    instr = nextInstr(cpu, ram);
-    if (instr == nullptr)
-        return 1;
+    instr = this->getNextInstr();
+    ASSERT_NE(instr, nullptr);
 
-    cpu.Execute(instr);
-
-    if (regs.ReadRegVal(12) != (uint32_t)-1)
-    {
-        std::cout << "got: " << (int32_t)regs.ReadRegVal(12) << "wanted: -1" << std::endl;
-    }
+    cpu->Execute(instr);
+    ASSERT_EQ(regs.ReadRegVal(12), (uint32_t)-1);
 
     std::cout << "testing: "
               << "sub x12, x12, x10" << std::endl;
-    instr = nextInstr(cpu, ram);
-    if (instr == nullptr)
-        return 1;
+    instr = this->getNextInstr();
+    ASSERT_NE(instr, nullptr);
 
-    cpu.Execute(instr);
+    cpu->Execute(instr);
+    ASSERT_EQ(regs.ReadRegVal(12), 0);
+}
 
-    if (regs.ReadRegVal(12) != 0)
-    {
-        std::cout << "got: " << regs.ReadRegVal(12) << "wanted: 0" << std::endl;
-    }
+int main(int argc, char** argv)
+{
 
-    return 0;
+    testing::InitGoogleTest(&argc, argv);
+
+    return RUN_ALL_TESTS();
 }
